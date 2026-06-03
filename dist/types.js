35f73ea39304e1bd302720f4a5f820457d6df01f
Nick24234 <114647778+Nick24234@users.noreply.github.com>
2026-06-03 06:51:03 +0300

    Delete dist directory

--- dist/types.js
+++
@@ -1,7 +0,0 @@
-export var Priority;
-(function (Priority) {
-    Priority["Low"] = "Low";
-    Priority["Medium"] = "Medium";
-    Priority["High"] = "High";
-    Priority["Critical"] = "Critical";
-})(Priority || (Priority = {}));
